3fb4aaae5bbea5d45e2c64bde11b97018d243281
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-08-24 00:57:38 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -5,22 +5,26 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities', cascade='all, delete')
     else:
+        name = ""
+        state_id = ""
+
+        __tablename__ = "cities"
+
         @property
         def places(self):
             """getter for places"""
-            from models import storage
-            from models.place import Place
             place_list = []
-            for place in storage.all(Place).values():
+            all_places = models.storage.all(models.place.Place)
+            for place in all_places.values():
                 if place.city_id == self.id:
                     place_list.append(place)
             return place_list
